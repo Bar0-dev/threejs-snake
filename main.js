83f0403fedc4ee516e5f10185d19ebbc4e131d4f
Bartosz <bartoszpietrzak.dev@gmail.com>
2024-02-14 09:49:57 +0100

    changed geometry and direction logic

--- main.js
+++ main.js
@@ -1,30 +1,32 @@
 import WebGL from 'three/addons/capabilities/WebGL.js';
 import * as THREE from 'three';
-import {SnakeSegment} from './src/components/SnakeSegment.js'
+import {Snake, SnakeHead} from './src/components/SnakeSegment.js'
 
 // Basic instances
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer();
+const light = new THREE.AmbientLight( 0xffffff, 1 ); // soft white light
+scene.add( light );
 
 // Renderer settings
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-let segment = new SnakeSegment(5, 0xff0033)
+let snake = new Snake(5, 0x802c22, 0.1, 4, scene)
 // Scene settings
-scene.add( segment.segment );
+// scene.add( ...snake.allSegments );
 
 // Camera settings
-camera.position.set(0, 0, 20);
+camera.position.set(10, 10, 50);
 camera.lookAt(0, 0, 0);
 
-document.addEventListener('keypress', (e)=>{segment.changeDirection(e.code)})
+document.addEventListener('keypress', snake.head.changeDirection.bind(snake.head))
 
 function animate() {
 	requestAnimationFrame( animate );
 
-	segment.move();
+	snake.moveAll();
 
 	renderer.render( scene, camera );
 }
--- src/components/SnakeSegment.js
+++ src/components/SnakeSegment.js
@@ -1,38 +1,82 @@
 import * as THREE from 'three';
 
-export class SnakeSegment {
-    constructor(size, color) {
+class GenericComponent {
+    constructor(size, color, speed) {
         this.size = size;
         this.color = color;
-        this.speed  = 0.1;
+        this.speed  = speed;
         this.direction = new THREE.Vector3( 1, 0, 0 );
-        this.geometry = new THREE.BoxGeometry( this.size, this.size, this.size );
-        this.material = new THREE.MeshBasicMaterial( { color: this.color } );
-        this.segment = new THREE.Mesh( this.geometry, this.material );
     }
-    
-    changeDirection(keyCode) {
-        switch (keyCode) {
-            case 'KeyW':
-                this.direction.set(0, 1, 0);
-                break;
-                
-            case 'KeyS':
-                this.direction.set(0, -1, 0);
-                break;
+}
 
-            case 'KeyA':
-                this.direction.set(-1, 0, 0);
-                break;
-            
-            case 'KeyD':
-                this.direction.set(1, 0, 0);
-                break;
-        }
+class SnakeSegment extends GenericComponent{
+    constructor(size, color, speed) {
+        super(size, color, speed);
+        this.geometry = new THREE.CapsuleGeometry( this.size/2, this.size/4, this.size, this.size );
+        this.material = new THREE.MeshStandardMaterial( { color: this.color, defines: { 'STANDARD': ''}, metalness:0.2, roughness:0.7 } );
+        this.body = new THREE.Mesh( this.geometry, this.material );
+    }
+
+    updateDirection(newDirection) {
+        this.direction.set(...newDirection);
     }
 
     move() {
-        this.segment.translateOnAxis(this.direction, this.speed)
+        this.body.translateOnAxis(this.direction, this.speed);
     }
 
 }
+
+class SnakeHead extends SnakeSegment {
+    constructor(size, color, speed) {
+        super(size, color, speed);
+        this.lastPositionToChangeDirection = new THREE.Vector3(0,0,0);
+        this.vectorLookup = {
+            KeyW: [0, 1, 0],
+            KeyS: [0, -1, 0],
+            KeyA: [-1, 0, 0],
+            KeyD: [1, 0, 0],
+        };
+    }
+
+    changeDirection(e) {
+        if (e.code in this.vectorLookup) {
+            this.direction = new THREE.Vector3(...this.vectorLookup[e.code]);
+            this.lastPositionToChangeDirection = this.body.position.clone();
+        }
+    }
+}
+
+class Snake extends GenericComponent{
+    constructor(size, color, speed, numberOfSegments, scene) {
+        super(size, color, speed);
+        this.numberOfSegments = numberOfSegments;
+        this.scene = scene;
+        this.head = new SnakeHead(this.size, this.color, this.speed);
+        this.gap = this.size/5
+        this.segments = [this.head];
+        this.spawnSegments(this.numberOfSegments);
+    }
+
+    spawnSegments(n) {
+        this.scene.add(this.head.body)
+        for (let i = 1; i<n; i++) {
+            const segment = new SnakeSegment(this.size, this.color, this.speed);
+            segment.body.translateOnAxis(this.direction.clone().negate(), (this.size+this.gap)*i)
+            this.segments.push(segment);
+            this.scene.add(segment.body)
+        }
+    }
+
+    moveAll() {
+        for (let i in this.segments) {
+            this.segments[i].move();
+            let substrat = this.head.lastPositionToChangeDirection.clone().sub(this.segments[i].body.position).length()
+            if (i>=1 && substrat < 0.01){
+                this.segments[i].direction = this.segments[i-1].direction;
+            }
+        }
+    }
+}
+
+export {SnakeSegment, SnakeHead, Snake}
